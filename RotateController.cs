36ae47ab119a7e1f7ba434cd628614d49931ca68
Jan <johnderlukiewicz@gmail.com>
2020-02-14 14:10:14 +0100

    Initial

---
+++ RotateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour
+{
+    public float yRotation = 5f;
+    public float rotationSpeed = 4f;
+    public bool changeDirections;
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!changeDirections)
+        {
+            yRotation += Time.deltaTime * rotationSpeed;
+            transform.rotation = Quaternion.Euler(0, yRotation, 0);
+            if (yRotation > 360)
+                yRotation = transform.rotation.y;
+        }
+        else
+        {
+            yRotation -= Time.deltaTime * rotationSpeed;
+            transform.rotation = Quaternion.Euler(0, yRotation, 0);
+            if (yRotation < -360)
+                yRotation = transform.rotation.y;
+        }
+
+    }
+}
